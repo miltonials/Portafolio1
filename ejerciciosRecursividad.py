5c4391347c85b9c8bf9c86ef52e32a7aed252e29
Milton Barrera <miltonjosue2001@gmail.com>
2021-03-17 22:23:36 -0600

    ejercicio division sin operador de division

--- ejerciciosRecursividad.py
+++ ejerciciosRecursividad.py
@@ -22,12 +22,15 @@ def divisores_aux(num, divisor):
         print(divisor)
     return divisores_aux(num, divisor - 1)
 
+
 """
 •Nombre: multiplicarRecusivo
 •Entrada: num, factor ( ambos números enteros)
 •Salida: multiplicación de num por el factor sin utilizar el operador de multiplicacioón
 •Restricciones: las entradas deben ser número enteros
 """
+
+
 def multiplicarRecusivo(num, factor):
     if(isinstance(num, int) and isinstance(factor, int)):
         if(factor == 0):
@@ -38,3 +41,29 @@ def multiplicarRecusivo(num, factor):
             return num + multiplicarRecusivo(num, factor - 1)
     else:
         print("Error: debes digitar un número entero")
+
+
+"""
+•Nombre: divisionRecusivo
+•Entrada: divisor y dividendo (números enteros)
+•Salida: división entera de las entradas sin utilizar el operador de división 
+•Restricciones: en las entradas el divisor debe ser entero y el dividendo entero diferente a 0
+"""
+
+
+def divisionRecusivo(divisor, dividendo):
+    if (isinstance(divisor, int) and isinstance(dividendo, int)):
+        if(dividendo == 0):
+            print("Las divisiones entre 0 no están definidas")
+        else:
+            return divisionRecusivo_aux(divisor, dividendo)
+    else:
+        print("Error. Los números deben ser enteros")
+        return -1
+
+
+def divisionRecusivo_aux(divisor, dividendo):
+    if(divisor-dividendo < 0):
+        return 0
+    else:
+        return 1 + divisionRecusivo_aux(divisor - dividendo, dividendo)
